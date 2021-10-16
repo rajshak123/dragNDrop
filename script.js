f122d44508b9828fb2a4ea175117e1aae6b312ab
rs <rs@localhost.localdomain>
2021-10-16 14:25:54 +0530

    jira like drag & drop vanilla js

---
+++ script.js
@@ -0,0 +1,25 @@
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = document.elementFromPoint(e.clientX,e.clientY).closest('.draggable')
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+})
